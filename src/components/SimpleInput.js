f1d0096e2aef74076bbe27daf7795ba96f4f7cf8
WizardIsHere <mshushant235@gmail.com>
2022-07-28 12:14:12 +0530

    🚧  Basic Validation Added

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,48 @@
+import React,{ useState} from 'react';
+
+const SimpleInput = (props) => {
+
+  const [enteredValue, setEnteredValue] = useState('')
+  const [fieldTouched, setFieldTouched] = useState(false)
+
+  const enteredValueIsValid = enteredValue.trim() !== '' ;
+  const inputIsInvalid = !enteredValueIsValid && fieldTouched;
+
+  const handleChange = (event) => {
+    setEnteredValue(event.target.value);
+    
+  }
+
+  const inputBlurHandler = (event) => {
+    setFieldTouched(true);
+    }
+    
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    setFieldTouched(true);
+    if(!enteredValueIsValid){
+      return
+    }
+    console.log(enteredValue);
+
+    setEnteredValue('');
+    setFieldTouched(false); 
+  }
+  
+  const inputClasses = inputIsInvalid ? 'form-control invalid' :'form-control ' ;
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={inputClasses} >
+        <label htmlFor='name' >Type Something..</label>
+        <textarea style={{width: '600px', height: '200px', border: '1px solid'}} type='text' id='name' onChange={handleChange} onBlur={inputBlurHandler}/>
+        {inputIsInvalid && <p className='error-text'>Field must not be empty!</p>}
+      </div>
+      <div className="form-actions">
+        <button onClick={formSubmitHandler}>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
